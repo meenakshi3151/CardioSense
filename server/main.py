27d604103dcf22f30ad8c3e9caccafed4b4794d7
Meenakshi Gupta <guptameenakshi957@gmail.com>
2025-10-16 10:53:19 +0530

    adding latest working version with gitignore file

--- server/main.py
+++ server/main.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 model = joblib.load('heart_disease_model.pkl')
 FEATURES = [
     'Age',
@@ -39,15 +41,18 @@ def home():
 def predict():
     try:
         data = request.get_json()
+        if not data:
+            return jsonify({"error": "No input data provided"}), 400
         missing = [f for f in FEATURES if f not in data]
         if missing:
             return jsonify({"error": f"Missing features: {missing}"}), 400
         input_df = pd.DataFrame([data], columns=FEATURES)
         prediction = model.predict(input_df)[0]
         result = {
-            "HeartDisease_Prediction": int(prediction),
+            "HeartDisease_Prediction": int(prediction)
         }
         return jsonify(result)
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
